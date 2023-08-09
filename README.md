74ed55bfbbb3aab9f3df4c938eb0882cf45c9d1e
Abhijit <artistrup07@gmail.com>
2023-08-09 22:17:35 +0530

    BIG CHANGE!

--- README.md
+++ README.md
@@ -1,21 +1,18 @@
 ﻿# Assignment-Generator
  <img width="913" alt="Screenshot 2023-08-05 090424" src="https://github.com/Abhijit004/Assignment-Generator/assets/133393475/ed250804-baf5-4c93-ac27-eb1dcd4317ba">
 
-A small Python program to take filename inputs and generate a Assignment report out of them with html and css.
+A small C program to take filename inputs and generate a Assignment report out of them with html and css.
 ## Aim behind this Program :gem:
 When it comes to creating a proper report of any assignment(containing codes and all), I wished if there could exist a program which would take my filepaths as input and generate the report for me, where every code block would be prettified(in word editor, a picture insertion ~ destruction :sweat:). Since I failed to find one, I created one for myself :sweat_smile:
 
 ## Some Implementation details :hammer_and_pick:
-* The program is heavily inspired by [Stackedit's](https://stackedit.io) Markdown to HTML rendering. I highly recommend using **Stackedit** for any markdown related works (I am writing this `README.md` using Stackedit only :grin:)
-
-* I used Python [Pygments](https://pygments.org/) module to accomplish the task of prettifying the code. After finding out that the builtin CLexer is not rendering as per my wish, I had to create one for myself, where, without [ChatGPT](https://chat.openai.com/) it would have taken a huge amount of my time.
-*  The program generates a **report.html file**. One needs to open it via any web browser and use the `print to pdf` functionality or use [sejda](https://www.sejda.com/html-to-pdf) to convert that HTML to a pdf (I recommend the latter as it won't break your HTML into pages).
-
+* The `c` file does the heavy lifting of taking input from the user and generating stylised html as per it. 
+* Styles of the html file are heavily inspired by [StackEdit](https://stackedit.io/), a great online markdown editor. Infact, this README is created in Stackedit's editor only :grin:!
+* I am using [Prism](https://prismjs.com/),  a lightweight, extensible syntax highlighter for highlighting the code blocks, it works stunningly great !
 ## Usage :computer:
-Download the `toHTML.py` file from github, open terminal in the same directory where it is downloaded, and run the following command (please, first install `Python`)
+Run the c file `toHTML.c`. Thats it !
 ```bash
-pip install Pygments
-python toHTML.py
+gcc toHTML.c -o toHTML && ./toHTML
 ```
 
 ## Detailed working process :gear:
---
+++ toHTML.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void replaceSubstring(char *str, const char *oldSubstr, const char *newSubstr) {
+    int oldLen = strlen(oldSubstr);
+    int newLen = strlen(newSubstr);
+    char *ptr = strstr(str, oldSubstr);
+    while (ptr != NULL) {
+        // Calculate the length of the remaining string
+        int remainingLen = strlen(ptr + oldLen);
+
+        // Shift the remaining characters to accommodate the new substring
+        memmove(ptr + newLen, ptr + oldLen, remainingLen + 1);
+
+        // Copy the new substring
+        memcpy(ptr, newSubstr, newLen);
+
+        // Search for the next occurrence
+        ptr = strstr(ptr + newLen, oldSubstr);
+    }
+}
+
+int main() {
+    char name[100];
+    char group[100];
+    char roll[100];
+    char assignmentNo[100];
+    int qno;
+
+    // User input
+    printf("Name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strcspn(name, "\n")] = '\0';
+
+    printf("Group: ");
+    fgets(group, sizeof(group), stdin);
+    group[strcspn(group, "\n")] = '\0';
+
+    printf("Roll No: ");
+    fgets(roll, sizeof(roll), stdin);
+    roll[strcspn(roll, "\n")] = '\0';
+
+    printf("Assignment No: ");
+    fgets(assignmentNo, sizeof(assignmentNo), stdin);
+    assignmentNo[strcspn(assignmentNo, "\n")] = '\0';
+
+    printf("No of Questions: ");
+    scanf("%d", &qno);
+    getchar(); // Consume the newline character
+
+    // Construct HTML code
+    char *htmlcode;
+    int bufferSize = 10000; // Adjust buffer size as needed
+    htmlcode = (char *)malloc(bufferSize);
+    snprintf(htmlcode, bufferSize,
+             "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Code block rendering</title><link rel=\"stylesheet\" href=\"prism.css\"/><script src=\"prism.js\" defer></script></head><body class='stackedit'><div class='stackedit__html'>Name: %s<br>Group: %s<br>RollNo: %s<br><h1>&#128203;Assignment %s</h1>",
+             name, group, roll, assignmentNo);
+
+    // Menu driven input
+    for (int i = 1; i <= qno; i++) {
+        char qLine[1000];
+        char cfile[100];
+        char output[1000];
+
+        printf("\tQline: ");
+        fgets(qLine, sizeof(qLine), stdin);
+        qLine[strcspn(qLine, "\n")] = '\0';
+
+        printf("\tC file: ");
+        fgets(cfile, sizeof(cfile), stdin);
+        cfile[strcspn(cfile, "\n")] = '\0';
+
+        printf("\tPng files sep by space: ");
+        fgets(output, sizeof(output), stdin);
+        output[strcspn(output, "\n")] = '\0';
+
+        FILE *f = fopen(cfile, "r");
+        if (f == NULL) {
+            perror("Error opening file");
+            return 1;
+        }
+        char text[10000]; // Adjust buffer size as needed
+        fread(text, 1, sizeof(text), f);
+        replaceSubstring(text, "<", "&lt;");
+        replaceSubstring(text, ">", "&gt;");
+        replaceSubstring(text, ";", "&semi;");
+        fclose(f);
+
+        char temp[10000]; // Adjust buffer size as needed
+        snprintf(temp, sizeof(temp), "<h3>%s</h3><p><strong>Code</strong></p><pre class='line-numbers'><code class='language-c'>%s</code></pre><p><strong>Output</strong></p>", qLine, text);
+        strcat(htmlcode, temp);
+
+        char *token = strtok(output, " ");
+        while (token != NULL) {
+            snprintf(temp, sizeof(temp), "<img src=\"%s\">", token);
+            strcat(htmlcode, temp);
+            token = strtok(NULL, " ");
+        }
+        strcat(htmlcode, "<hr>");
+        printf("\n");
+    }
+
+    strcat(htmlcode, "</div></body></html>");
+
+    // Writing to file
+    FILE *outputFile = fopen("report.html", "w");
+    if (outputFile == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+    fputs(htmlcode, outputFile);
+    fclose(outputFile);
+
+    free(htmlcode);
+    printf("Process completed, please find a file named report.html on this directory\n");
+
+    return 0;
+}
