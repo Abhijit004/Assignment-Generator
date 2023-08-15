816d7c4eb11a42aaf0a7d7e0e94a9617bc089778
Abhijit <artistrup07@gmail.com>
2023-08-15 10:54:26 +0530

    adding regex to highlight function headers

--- prism.js
+++ prism.js
@@ -1281,6 +1281,10 @@ Prism.languages.clike = {
 		pattern: /(["'])(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
 		greedy: true
 	},
+	'important': {
+		pattern: /(?<=\b\w+\s)\s?\b\w+\b\s?(?=\()/,
+		greedy: true
+	},
 	'class-name': {
 		pattern: /(\b(?:class|extends|implements|instanceof|interface|new|trait)\s+|\bcatch\s+\()[\w.\\]+/i,
 		lookbehind: true,
@@ -1288,7 +1292,7 @@ Prism.languages.clike = {
 			'punctuation': /[.\\]/
 		}
 	},
-	'keyword': /\b(?:break|catch|continue|do|else|finally|for|function|if|in|instanceof|new|null|return|throw|try|while)\b/,
+	'property': /\b(?:break|catch|continue|do|else|finally|for|function|if|in|instanceof|new|null|return|throw|try|while)\b/,
 	'boolean': /\b(?:false|true)\b/,
 	'function': /\b\w+(?=\()/,
 	'number': /\b0x[\da-f]+\b|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[+-]?\d+)?/i,
@@ -1629,4 +1633,3 @@ delete Prism.languages.c['boolean'];
 	});
 
 }());
-
